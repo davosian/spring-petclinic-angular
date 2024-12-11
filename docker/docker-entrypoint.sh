15d8caf3d1de066b4619679c394383b73b6c2913
Dennis Staiger <5349+davosian@users.noreply.github.com>
2024-12-11 12:51:10 +0100

    replaced image building logic

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+# Construct the BACKEND_URL
+# Use the variables passed from the ConfigMap or as env variables
+BACKEND_URL="${BACKEND_URL:-http://no-backend-url-set:80}"
+API_URL="${API_URL:-https://no-api-url-set/backend}"
+
+# Replace environment variables in the Nginx config template
+envsubst '$BACKEND_URL $API_URL' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
+# Execute the CMD (which is "nginx -g 'daemon off;'")
+exec "$@"
